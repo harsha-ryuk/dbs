5dee826e4ee1374d7afbf8e0b9a9f40ff6759ecf
anshika <sethas123@gmail.com>
2021-02-27 12:16:35 +0530

    added the model for the place order page

--- OrderBookSysteApp/models.py
+++ OrderBookSysteApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Search(models.Model) :
+    stock= models.CharField(max_length=100)
+    ordertype = models.CharField(max_length=10)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+
+
+
+
