7ceaeec99f22bdc5e48ba1323492f4ae69d5efc1
anshika <sethas123@gmail.com>
2021-02-27 12:46:00 +0530

    2nd admin page

--- OrderBookSysteApp/models.py
+++ OrderBookSysteApp/models.py
@@ -10,3 +10,13 @@ class Search(models.Model) :
 
 
 
+class adm(models.Model):
+    sno= models.IntegerField()
+    sname= models.CharField(max_length=100)
+    orderq= models.IntegerField()
+    ordert= models.CharField(max_length=100)
+    executeq= models.CharField(max_length=100)
+    pri = models.DoubleField()
+    order_sta= models.CharField()
+    order_date= models.DateTime.Field(auto_now= True)
+
